5b46a2f2a0833e9706b27dd8fa747c0f8c330346
tatsuya fukui <nakedtatsuya6@gmail.com>
2018-08-06 21:42:08 +0900

    deploy test

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const {mongoose} = require('../db/mongoose');
+
+const User = mongoose.model('User', {
+		name: {
+				type: String
+		},
+		age: {
+				type: Number
+		}
+});
+
+module.exports = {User};
---
+++ server/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+let hbs = require('hbs');
+const {User} = require('./models/user');
+const http = require('http');
+const socketIO = require('socket.io');
+let app = express();
+const port = process.env.PORT || 3000;
+
+let server = http.createServer(app);
+let io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'hbs');
+//静的フォルダの読み込みにはexpress.staticを使う
+app.use(express.static('public'));
+hbs.registerPartials("/Users/fukuitatsuya/Desktop/lunch-friend" + '/views/partials');
+
+
+
+var newUser = new User({
+		name: "tatsuya",
+		age: 22
+});
+
+newUser.save().then(doc => {
+		console.log('saved');
+});
+
+
+
+
+app.get('/', async (req, res) => {
+		let test = await User.findOne({name: "tatsuya"});
+		res.render('home.hbs', {
+				title: 'HOME PAGE',
+				user: test.name
+		});
+});
+
+app.get('/about', (req, res) => {
+		res.render('about.hbs', {
+				title: 'about'
+		});
+});
+
+app.get('/feed/:location', (req, res) => {
+		res.render('feed.hbs', {
+				title: req.params.location
+		});
+});
+
+app.post('/feed', (req, res) => {
+		res.redirect(`/feed/${req.body.location}`);
+});
+
+
+
+
+server.listen(port, () => {
+		console.log(`server start port ${port}`);
+});
