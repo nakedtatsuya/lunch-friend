e9feec979a7094be896253c283ca8975fa70ff0a
ebinan92 <ebinan92@outlook.jp>
2018-09-12 02:05:49 +0900

    認証機能追加(機能のみ)

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,3 @@
 const mongoose = require('mongoose');
-
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
-
-module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 require('./config/config');
 require('../gulpfile');
+let {mongoose} = require('./db/mongoose');
 const _ = require('lodash');
 const express = require('express');
 const path = require('path');
 let hbs = require('hbs');
-let {mongoose} = require('./db/mongoose');
-const {User} = require('./models/user');
+let cookieParser = require("cookie-parser");
+let bodyParser = require("body-parser");
+let flash = require("connect-flash");
+let session = require("express-session");
 const {Collect} = require('./models/collect');
+let User = require('./models/user');
 const http = require('http');
+let passport = require('passport')
+let LocalStrategy = require("passport-local").Strategy;
 const socketIO = require('socket.io');
 let app = express();
 const port = process.env.PORT;
@@ -18,6 +24,8 @@ let io = socketIO(server);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
+app.use(flash());
+
 //静的フォルダの読み込みにはexpress.staticを使う
 app.use(express.static('public'));
 hbs.registerPartials(partialsPath);
@@ -30,12 +38,94 @@ hbs.registerHelper('getPostTime', function(timestamp) {
 		);
 });
 
+// passport が ユーザー情報をシリアライズすると呼び出されます
+passport.serializeUser(function (id, done) {
+    done(null, id);
+});
+
+// passport が ユーザー情報をデシリアライズすると呼び出されます
+passport.deserializeUser(function (id, done) {
+    User.findById(id, (error, user) => {
+        if (error) {
+            return done(error);
+        }
+        done(null, user);
+    });
+});
+
+// passport における具体的な認証処理を設定します。
+passport.use(
+    "local-login",
+    new LocalStrategy({
+        usernameField: "username",
+        passwordField: "password",
+        passReqToCallback: true
+    }, function (req, username, password, done) {
+        process.nextTick(() => {
+        	console.log(username,"username");
+
+            User.findOne({ name: username}, function (error, user) {
+            	console.log(user,"db_username")
+
+                if (error) {
+                    return done(error);
+                }
+                if (!user) {
+                    return done(null, false, { message: 'ユーザーIDが正しくありません。' });
+                }
+                if (user.password !== password) {
+                    return done(null, false, {message: 'パスワードが正しくありません。'});
+                }
+                return done(null, user);
+            });
+        });
+    })
+);
+
+// ログインされているか判別
+function checkAuthentication(req,res,next){
+    	if(req.isAuthenticated()){
+        	//req.isAuthenticated() will return true if user is logged in
+        	next();
+    	} else{
+        	res.redirect("/login");
+    	}
+}
+
+// passport設定
+app.use(session({ secret: "some salt", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => {
 		//let test = await User.findOne({name: "tatsuya"});
 		res.render('home.hbs', {
 				title: 'HOME PAGE',
 				user: 'test'
 		});
+});
+
+//ログイン
+app.get('/login', (req,res) => {
+		res.render('login.hbs',{
+			title: 'login',
+			message: req.flash('error')
+		});
+});
+
+app.post('/login',passport.authenticate(
+    'local-login', {
+        successRedirect: "/collect",
+        failureRedirect: "/login",
+        failureFlash : true
+    })
+);
+
+//ログアウト
+app.get('/logout',(req,res) => {
+    res.render('logout.hbs',{
+        title: 'logout'
+    });
 });
 
 app.get('/collect', (req, res) => {
@@ -47,7 +137,7 @@ app.get('/collect', (req, res) => {
 app.post('/collect', async (req, res) => {
 		const body = _.pick(req.body, ['station', 'store', 'link', 'num', 'time', 'minute', 'comment']);
 		let collect = new Collect(body);
-		await collect.save().then(cllect => {
+		await collect.save().then(colect => {
 
 		}).catch(e => {
 				
